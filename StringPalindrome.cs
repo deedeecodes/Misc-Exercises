cf0b59b1f0f0b6427cc33b214d4cf7bb440233a6
diane <dianetodirica@gmail.com>
2021-11-30 04:15:52 -0800

    Refactor StringPalindrome

--- StringPalindrome.cs
+++ StringPalindrome.cs
@@ -8,26 +8,11 @@ namespace IttyBittyProblems
     public class StringPalindrome
     {
 
-        public bool IsPalindrome(string str, string? chars)
-        {
-            str = str.ToLower();
-            str = Regex.Replace(str, chars, "");
-
-            string reversed = "";
-
-            for (int i = str.Length - 1; i >= 0; i--)
-            {
-                reversed += str[i];
-            }
-
-            if (str.SequenceEqual(reversed))
-                return true;
-
-            return false;
-        }
-
         public bool IsPalindrome(string str)
         {
+            str = str.ToLower();
+            str = Regex.Replace(str, "[?!:;., \n\t]", "");
+
             string reversed = "";
 
             for (int i = str.Length - 1; i >= 0; i--)
