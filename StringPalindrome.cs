87989b39a46d4669a1df36c11446d0f51492c75e
diane <dianetodirica@gmail.com>
2021-11-30 03:49:30 -0800

    Add new problem

---
+++ StringPalindrome.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace IttyBittyProblems
+{
+    public class StringPalindrome
+    {
+
+        public bool IsPalindrome(string str, string? chars)
+        {
+            str = str.ToLower();
+            str = Regex.Replace(str, chars, "");
+
+            string reversed = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+
+            if (str.SequenceEqual(reversed))
+                return true;
+
+            return false;
+        }
+
+        public bool IsPalindrome(string str)
+        {
+            string reversed = "";
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                reversed += str[i];
+            }
+
+            if (str.SequenceEqual(reversed))
+                return true;
+
+            return false;
+        }
+    }
+}
